d91f9cf9a6d18e633f74c0561720181af60b14b8
Filippo Fontanelli <info@fontanellif.it>
2014-05-12 12:01:34 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-nprobe-ELK
-==========
+# What is Logstash?
+Logstash is a log indexer built on top of elasticsearch. It aggregates logs from multiple sources and allows you to query them using the [Apache Lucene query parser syntax](http://lucene.apache.org/core/2_9_4/queryparsersyntax.html).
+
+Logstash is built on elasticsearch, which allows your data to scale easily. If you run out of space, simply add a new elasticsearch node to your cluster. It's easy to scale with your data.
+
+# How it works
+Logstash has two parts, the indexer and the server. The indexer works on a specific datasource to collect logs and ship them to the server. Before shipping the logs you can do interesting things, such as mutate them, add tags, or disregard them altogether.
+
+Adding tags to certain types of logs allows you to quickly retrieve them and keep track of trending information.
+
+
+# The frontend
+While not a direct part of the logstash project, Kibana works on top of logstash to give you visualization and montoring tools. Kibana also gives you the flexibility to define patterns and filters and then watch the stream for these matches as they happen in realtime.
+
+![Alt text](/img/ntopng_kibana_dashboard.png "Ntopng kibana dashboard")
+# Setup
+The entire setup has been automated for Ubuntu. Simply run:
+
+```
+$ sudo ./ntopng_logstash
+```
+
+To run logstash use the following command:
+
+```
+$ sudo ./logstash -h
+usage: ./logstash options
+
+This script run logstash
+
+OPTIONS:
+   -h      Show this message
+   -e      Elasticsearch configuration path. Default: /etc/elasticsearch/ (optional)
+   -l      Logstash configuration file. Default: /etc/logstash/logstash.conf (optional)
+   -w      Active logstash web UI.
+   -v      Verbose
+
+INFO:
+ In order to delete all elasticsearch index, please run this command while logstash is running: curl -XDELETE 'http://localhost:9200/'
+```
+
+Elasticsearch and logstash will be listening on their default port. Kibana will be listening on 9292 port.
+
+# Examples
+We have prepared few example of logstash configuration for different input types. You can find this examples in the examples folder.
---
+++ elk.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+#Provided by Filippo Fontanelli
+# This script will automatically install and configure Logstash whit embedded Elasticsearch and Kibana3
+
+echo 'Install Pre-Reqs and EL'
+
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+
+wget -O - http://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -
+
+echo 'deb http://packages.elasticsearch.org/elasticsearch/1.0/debian stable main' >> /etc/apt/sources.list
+echo 'deb http://packages.elasticsearch.org/logstash/1.4/debian stable main' >> /etc/apt/sources.list
+
+apt-get update
+apt-get install -y --force-yes default-jdk rubygems ruby1.9.1-dev libcurl4-openssl-dev git apache2 libzmq-dev
+apt-get install elasticsearch logstash
+sudo update-rc.d elasticsearch defaults 95 10
+
+echo 'Configuring Elasticsearch'
+# sed -i '$a\cluster.name: elk' /etc/elasticsearch/elasticsearch.yml
+# sed -i '$a\node.name: "elastic-master"' /etc/elasticsearch/elasticsearch.yml
+# sed -i '$a\node.master: true' /etc/elasticsearch/elasticsearch.yml
+# sed -i '$a\node.data: true' /etc/elasticsearch/elasticsearch.yml
+# sed -i '$a\path.data: /var/data/elasticsearch' /etc/elasticsearch/elasticsearch.yml
+# sed -i '$a\path.work: /var/data/elasticsearch/tmp' /etc/elasticsearch/elasticsearch.yml
+# sed -i '$a\path.logs: /var/log/elasticsearch' /etc/elasticsearch/elasticsearch.yml
+
+# sed -i '$a\index.number_of_shards: 1' /etc/elasticsearch/elasticsearch.yml
+# sed -i '$a\index.number_of_replicas: 0' /etc/elasticsearch/elasticsearch.yml
+# sed -i '$a\discovery.zen.ping.multicast.enabled: false' /etc/elasticsearch/elasticsearch.yml
+# sed -i '$a\discovery.zen.ping.unicast.hosts: ["127.0.0.1:[9300-9400]"]' /etc/elasticsearch/elasticsearch.yml
+# sed -i '$a\bootstrap.mlockall: true' /etc/elasticsearch/elasticsearch.yml
+
+
+echo 'Create grok pattern folder'
+mkdir -p /etc/logstash/patterns
+cd /tmp
+git clone https://github.com/logstash/logstash
+cp logstash/patterns/* /etc/logstash/patterns/
+rm -rf logstash
+echo 'Remember to set you patterns_dir to "/etc/logstash/patterns" (i.e. patterns_dir => "/etc/logstash/patterns")'
+
+################################ Kibana ################################
+
+echo 'Install and configure the Kibana frontend'
+cd /var/www
+wget https://download.elasticsearch.org/kibana/kibana/kibana-3.0.1.tar.gz
+tar zxvf kibana-*
+rm kibana-*.tar.gz
+mv kibana-* kibana
+
+# Install and configure the Kibana frontend
+echo 'Restart ELK'
+service logstash restart
+service elasticsearch restart
+
+# All Done
+echo "Installation has completed!!"
+echo -e "Connect to http://localhost/kibana"
